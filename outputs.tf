c013dc71c8da2e427ef94f7ccbee5d93db1e98e6
dramlin1010 <dramlin1010@g.educaand.es>
2025-06-03 09:55:19 +0200

    Preparando Output para Actions

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,10 @@ output "instance_public_ip" {
 
 output "ssh_command" {
   description = "Comando para conectar via SSH"
-  value       = "ssh -i ../${var.key_name}.pem ubuntu@${aws_eip.k3s_node_eip.public_ip}"
+  value       = "ssh -i ${var.key_name}.pem ubuntu@${aws_eip.k3s_node_eip.public_ip}"
 }
 
 output "kubeconfig_setup_command" {
   description = "Comando para copiar KUBECONFIG desde la instancia (ejecutar localmente)"
-  value       = "scp -i ../${var.key_name}.pem ubuntu@${aws_eip.k3s_node_eip.public_ip}:/home/ubuntu/.kube/config ~/.kube/config-${var.project_name} | kubectl --kubeconfig=/home/daniel/.kube/config-${var.project_name} get all"
+  value       = "scp -i ${var.key_name}.pem ubuntu@${aws_eip.k3s_node_eip.public_ip}:/home/ubuntu/.kube/config ~/.kube/config-${var.project_name} | kubectl --kubeconfig=/home/daniel/.kube/config-${var.project_name} get all"
 }
